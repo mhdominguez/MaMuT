e61a6d752f61ead7e9a619c2f4534ce8bc204307
mhdominguez <martin.h.dominguez@gmail.com>
2021-07-02 12:00:30 -0700

    modified:   SpotGroupNode.java

--- src/main/java/fiji/plugin/mamut/threedviewer/SpotGroupNode.java
+++ src/main/java/fiji/plugin/mamut/threedviewer/SpotGroupNode.java
@@ -250,7 +250,7 @@ public class SpotGroupNode< K > extends ContentNode
 			return;
 		}*/
 		//System.out.println( "[SpotGroupNode] makeMeshes, optionsProcessSpots:" + String.valueOf(optionsProcessSpots) );
-		//System.out.println( "[SpotGroupNode] makeMeshes, optionsProcessText:" + String.valueOf(optionsProcessText) );
+		System.out.println( "[SpotGroupNode] makeMeshes, optionsProcessText:" + String.valueOf(optionsProcessText) );
 		
 		for ( final K key : centers.keySet() )
 		{
